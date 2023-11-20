a8d5874d92328d73e1e6410c5c8cc3c2fe76f2ed
MKGeme <128739786+MKGeme@users.noreply.github.com>
2023-11-20 21:08:02 +0200

    Final Commit

--- Assignment_2_Cloud.py
+++ Assignment_2_Cloud.py
@@ -41,7 +41,41 @@ exchange_data = requests.get(exchange_url).json()
 for currency in target_currencies:
     if currency != base_currency:
         st.write(f"{base_currency} to {currency}: {exchange_data['rates'][currency]}")
+st.header('AWS Lambda Weather Data')
+city_lambda = st.text_input("Enter a city for Lambda Weather:", key='city_lambda')
 
+if st.button("Fetch Weather from AWS Lambda"):
+    if city_lambda:
+        weather_payload = {
+            "weather_city": city_lambda
+        }
+
+        # Start time for calculating the request duration
+        weather_request_start_time = time.time()
+
+        # Send request to Lambda
+        lambda_weather_response = requests.post("https://x0bxdl2bmg.execute-api.eu-north-1.amazonaws.com/prod/rates", json=weather_payload)
+
+        # End time for request duration calculation
+        weather_request_end_time = time.time()
+        weather_request_duration = weather_request_end_time - weather_request_start_time
+
+        if lambda_weather_response.status_code == 200:
+            # Parse the JSON string from the response body
+            lambda_response_body = json.loads(lambda_weather_response.json()['body'])
+
+            # Check if the expected keys are in the parsed data
+            if 'main' in lambda_response_body and 'temp' in lambda_response_body['main']:
+                temp_celsius = lambda_response_body['main']['temp'] - 273.15
+                st.write(f"Temperature in {city_lambda}: {temp_celsius:.2f}°C")
+                st.write(f"Weather: {lambda_response_body['weather'][0]['description']}")
+                st.write(f"Request duration: {weather_request_duration:.2f} seconds")
+            else:
+                st.error("Weather data not found in the response.")
+        else:
+            st.error("Error fetching weather data from AWS Lambda.")
+    else:
+        st.error("Please enter a city name.")
 # Section for AWS Lambda Exchange Rate
 st.header('AWS Lambda Exchange Rate')
 base_currency_lambda = st.selectbox("Select base currency for Lambda:", currencies, key='base_lambda')
